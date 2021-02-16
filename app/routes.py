a0af214f0d1b8f7a9b8464c8f7f5f8f66079c02d
Viktar Bychko <viktarbychko92@gmail.com>
2021-02-16 17:11:15 +0300

    stage 08

--- app/routes.py
+++ app/routes.py
@@ -97,4 +97,34 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', 
                             form=form)
-        
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash("You can't follow yourself!")
+        return redirect(url_for('index'))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are following {}!'.format(username))
+    return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash("You can't unfollow yourself!")
+        return redirect(url_for('index'))
+    current_user.unfollow(username)
+    db.session.commit()
+    flash('You are not unfollowing {}!'.format(username))
+    return redirect(url_for('user', username=username))
